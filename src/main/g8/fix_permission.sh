c36b9701d525b3089c7819ec5230ffe894962ac5
Shota Fukumori (sora_h) <sorah@tubusu.net>
2012-05-02 00:45:03 +0900

    add fix_permission.sh

---
+++ src/main/g8/fix_permission.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+chmod -x .gitignore
+chmod -x AndroidManifest.xml
+chmod -x README.md
+chmod -x build.sbt
+chmod -x default.properties
+chmod -x project/build.properties
+chmod -x project/plugins.sbt
+chmod -x res/layout/main.xml
+chmod -x res/values/strings.xml
+chmod -x src/main/scala/Activity.scala
+chmod -x src/test/scala/ActivitySpec.scala
